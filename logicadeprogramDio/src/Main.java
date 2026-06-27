3d6e2cdeb2eec8ebe42a6f87b82e2fd054da9722
JoeMateus <jonnymateusuni14@gmail.com>
2026-06-27 10:28:51 -0300

    Troquei Swicht/Case por If/Else, achei que se encaixa melhor. Além disso, usei vetores para facilitar na hora de escolha do herói.

--- logicadeprogramDio/src/Main.java
+++ logicadeprogramDio/src/Main.java
@@ -3,17 +3,29 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        String nome = "Supermen";
-        String nome2 = "Batman";
-        String nome3 = "Flash";
-        String nome4 = "Supergirl";
+        System.out.println("Escolha um persornagem:");
+        String[] nome = {"Supermen", "Batman", "Flash", "SuperGirl"};
+        System.out.println(nome[3]);
+        int xp = 7000;
+        String nivel;
 
-        int XP = 7000;
-
-        switch (XP) {
-            case1:   1000:
-            System.out.println("Ferro");
+        if (xp < 1000) {
+            nivel = "Ferro";
+        } else if (xp <= 2000) {
+            nivel = "Bronze";
+        } else if (xp <= 5000) {
+            nivel = "Prata";
+        } else if (xp <= 7000) {
+            nivel = "Ouro";
+        } else if (xp <= 8000) {
+            nivel = "Platina";
+        } else if (xp <= 9000) {
+            nivel = "Ascendente";
+        } else if (xp <= 10000) {
+            nivel = "Imortal";
+        } else {
+            nivel = "Radiante";
         }
-
+        System.out.println("O herói " + (nome[3]) +  "está no nível " + nivel);
     }
 }
