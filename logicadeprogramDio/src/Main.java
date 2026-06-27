f8b4c1ae71436135e8bfae11b32ba38bf9f47dc4
JoeMateus <jonnymateusuni14@gmail.com>
2026-06-27 08:08:51 -0300

    adicionei as variaveis e estou adicionando o os case

--- logicadeprogramDio/src/Main.java
+++ logicadeprogramDio/src/Main.java
@@ -3,5 +3,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        String nome = "Supermen";
+        String nome2 = "Batman";
+        String nome3 = "Flash";
+        String nome4 = "Supergirl";
+
+        int XP = 7000;
+
+        switch (XP) {
+            case1:   1000:
+            System.out.println("Ferro");
+        }
+
     }
 }
