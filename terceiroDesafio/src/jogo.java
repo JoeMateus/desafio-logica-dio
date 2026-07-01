de3a01516f06dd982f227970ed3927c7b1774502
JoeMateus <jonnymateusuni14@gmail.com>
2026-06-30 21:01:17 -0300

    aqui adicionei as proriedades

---
+++ terceiroDesafio/src/jogo.java
@@ -0,0 +1,12 @@
+public class jogo {
+
+    private String nome;
+    private int idade;
+    private String tipo;
+
+    public jogo(String nome, int idade, String tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+}
