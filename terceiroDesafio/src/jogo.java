fa5cc15b4cdc398c17872535f0e7f6d08578c852
JoeMateus <jonnymateusuni14@gmail.com>
2026-06-30 21:03:54 -0300

    Usei como opçoes o swicth-case

--- terceiroDesafio/src/jogo.java
+++ terceiroDesafio/src/jogo.java
@@ -9,4 +9,26 @@ public class jogo {
         this.idade = idade;
         this.tipo = tipo;
     }
+    public void atacar(){
+        String atacar;
+
+        switch (tipo.toLowerCase()) {
+            case "mago":
+                atacar = "magia";
+                break;
+            case "guerreiro":
+                atacar = "espada";
+                break;
+            case " monge":
+                atacar = "artes marciais";
+                break;
+            case "ninja ":
+                atacar = "shuriken";
+                break;
+            default:
+                atacar = "ataque desconhecido";
+        }
+        System.out.println("Usou " + tipo + "e atacou usando " +atacar);
+    }
 }
+
